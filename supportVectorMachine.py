3165c05e569f1eec195821a18a371c0989bf8107
tr4nce <tr4ncepl@gmail.com>
2020-04-02 14:12:59 +0200

    Implementation of SVM algorithm

---
+++ supportVectorMachine.py
@@ -0,0 +1,155 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils import check_random_state
+from sklearn.preprocessing import LabelEncoder
+
+
+def projection_simplex(v, z=1):
+
+
+    n_features = v.shape[0]
+    u = np.sort(v)[::-1]
+    cssv = np.cumsum(u) - z
+    ind = np.arange(n_features) + 1
+    cond = u - cssv / ind > 0
+    rho = ind[cond][-1]
+    theta = cssv[cond][-1] / float(rho)
+    w = np.maximum(v - theta, 0)
+    return w
+
+
+class MulticlassSVM(BaseEstimator, ClassifierMixin):
+
+    def __init__(self, C=1, max_iter=50, tol=0.05,
+                 random_state=None, verbose=0):
+        self.C = C
+        self.max_iter = max_iter
+        self.tol = tol,
+        self.random_state = random_state
+        self.verbose = verbose
+
+    def _partial_gradient(self, X, y, i):
+
+        g = np.dot(X[i], self.coef_.T) + 1
+        g[y[i]] -= 1
+        return g
+
+    def _violation(self, g, y, i):
+
+        smallest = np.inf
+        for k in range(g.shape[0]):
+            if k == y[i] and self.dual_coef_[k, i] >= self.C:
+                continue
+            elif k != y[i] and self.dual_coef_[k, i] >= 0:
+                continue
+
+            smallest = min(smallest, g[k])
+
+        return g.max() - smallest
+
+    def _solve_subproblem(self, g, y, norms, i):
+
+        Ci = np.zeros(g.shape[0])
+        Ci[y[i]] = self.C
+        beta_hat = norms[i] * (Ci - self.dual_coef_[:, i]) + g / norms[i]
+        z = self.C * norms[i]
+
+
+        beta = projection_simplex(beta_hat, z)
+
+        return Ci - self.dual_coef_[:, i] - beta / norms[i]
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+
+
+        self._label_encoder = LabelEncoder()
+        y = self._label_encoder.fit_transform(y)
+
+
+        n_classes = len(self._label_encoder.classes_)
+        self.dual_coef_ = np.zeros((n_classes, n_samples), dtype=np.float64)
+        self.coef_ = np.zeros((n_classes, n_features))
+
+
+        norms = np.sqrt(np.sum(X ** 2, axis=1))
+
+
+        rs = check_random_state(self.random_state)
+        ind = np.arange(n_samples)
+        rs.shuffle(ind)
+
+        violation_init = None
+        for it in range(self.max_iter):
+            violation_sum = 0
+
+            for ii in range(n_samples):
+                i = ind[ii]
+
+
+                if norms[i] == 0:
+                    continue
+
+                g = self._partial_gradient(X, y, i)
+                v = self._violation(g, y, i)
+                violation_sum += v
+
+                if v < 1e-12:
+                    continue
+
+
+                delta = self._solve_subproblem(g, y, norms, i)
+
+
+                self.coef_ += (delta * X[i][:, np.newaxis]).T
+                self.dual_coef_[:, i] += delta
+
+            if it == 0:
+                violation_init = violation_sum
+
+            vratio = violation_sum / violation_init
+
+            if self.verbose >= 1:
+                print("iter", it + 1, "violation", vratio)
+
+            if vratio < self.tol:
+                if self.verbose >= 1:
+                    print("Converged")
+                break
+
+        return self
+
+    def predict(self, X):
+        decision = np.dot(X, self.coef_.T)
+        pred = decision.argmax(axis=1)
+        return self._label_encoder.inverse_transform(pred)
+
+
+if __name__ == '__main__':
+    from sklearn.datasets import load_iris
+
+    np.set_printoptions(suppress=True)
+    data =pd.read_csv('data.csv')
+
+    x1 = data.drop('class', axis = 1)
+    y1 = data['class']
+
+    x2 = np.array(x1)
+    y2 = np.array(y1)
+
+    print(x2.shape)
+    print(y2.shape)
+
+
+    iris = load_iris()
+    X, y = iris.data, iris.target
+
+    print(X.shape)
+    print(y.shape)
+
+
+    clf = MulticlassSVM(C=1, tol=0.00001, max_iter=10, random_state=0, verbose=1)
+    clf.fit(x2, y2)
+    print(clf.score(x2, y2))
